31036de28a65a7b1c853e981213ef0d137592822
Lena Zhang <LenaZhang@Lenas-MBP-2.fios-router.home>
2020-08-12 14:53:28 -0400

    Add Items Table

--- mrfmtracker_api/init.sql
+++ mrfmtracker_api/init.sql
@@ -1,3 +1,19 @@
 /* SQL STATESMENT FOUND BELOW WILL ALLOW US TO
    POPULATE THE DATABASE WITH TEST VALUES */
 
+Create TABLE [mrfmtracker.] [schema_name.] table_name
+       pk_column data_type Primary Key, 
+       column_1 datatype NOT null, column_2 data_type,
+       ...,
+       table_constraints);
+
+CREATE TABLE ITEMS (
+      name INT PRIMARY KEY INDENTITY (1,1),
+      Name varchar(50) NOT NULL,
+      Text varchar(50) NOT NULL, 
+      Category varchar(50) NOT NULL, 
+      
+
+
+
+)
