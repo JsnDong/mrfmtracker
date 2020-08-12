f45e4ed33e52f8ea2e0cdf92676e2913b1559912
Jason Dong <jasondong1294@gmail.com>
2020-08-12 12:50:37 -0400

    sightings table and testing index

--- mrfmtracker_api/init.sql
+++ mrfmtracker_api/init.sql
@@ -1,3 +1,14 @@
 /* SQL STATESMENT FOUND BELOW WILL ALLOW US TO
    POPULATE THE DATABASE WITH TEST VALUES */
 
+CREATE TABLE sightings (
+   sightingID SERIAL PRIMARY KEY,
+   item TEXT REFERENCES items(name),
+   stats JSON, 
+   creator INTEGER REFERENCES accounts(accountID),
+   sightingDate DATE NOT NULL,
+   price integer NOT NULL,
+   quantity integer,
+   seller VARCHAR(20),
+   location JSON 
+);
