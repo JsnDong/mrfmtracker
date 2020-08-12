3f7b8796ade4e0d3eefe1407d203222e664285ef
Cecilia <cecilia@Cecilias-Air.lan>
2020-08-12 14:14:25 -0400

    <i added accounts and characters into init.sql>

--- mrfmtracker_api/init.sql
+++ mrfmtracker_api/init.sql
@@ -1,3 +1,17 @@
 /* SQL STATESMENT FOUND BELOW WILL ALLOW US TO
    POPULATE THE DATABASE WITH TEST VALUES */
+CREATE TABLE accounts (
+   accountID SERIAL PRIMARY KEY,
+   username VARCHAR(20)
+   password VARCHAR(20)
+   email VARCHAR(320)
 
+)
+
+CREATE TABLE characters (
+   name VARCHAR(20)
+   level integer
+   job CHAR(10)
+   storeLocation VARCHAR(20)
+
+)
