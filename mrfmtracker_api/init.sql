9874472b239c3c7f1b5dd3c1037ed94c783a11d5
David <dwong5@babson.edu>
2020-08-12 14:13:16 -0400

    listing table

--- mrfmtracker_api/init.sql
+++ mrfmtracker_api/init.sql
@@ -1,3 +1,12 @@
 /* SQL STATESMENT FOUND BELOW WILL ALLOW US TO
    POPULATE THE DATABASE WITH TEST VALUES */
 
+CREATE TABLE listing (
+   listingID SERIAL PRIMARY KEY,
+   item TEXT REFERENCES items(name),
+   stats JSON,
+   seller TEXT REFERENCES characters(name),
+   price integer NOT NULL,
+   quantity integer,
+   location JSON
+);
