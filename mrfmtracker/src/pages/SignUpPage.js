cec71acebf74ac2cb0b2a1c8afe69d282a5f71ae
Jason Dong <jasondong1294@gmail.com>
2020-09-21 01:17:25 -0400

    added basic validation to signup page inputs

--- mrfmtracker/src/pages/SignUpPage.js
+++ mrfmtracker/src/pages/SignUpPage.js
@@ -4,6 +4,8 @@ import {
     Link
 } from 'react-router-dom';
 
+import * as Validation from '../tools/Validation.js';
+
 class SignUpPage extends React.Component {
     constructor(props) {
         super();
@@ -11,7 +13,13 @@ class SignUpPage extends React.Component {
             email: '',
             username: '',
             password: '',
-            confirmPassword: ''
+            confirmPassword: '',
+            errors: {
+                email: null,
+                username: null,
+                password: null,
+                confirmPassword: null,
+            }
         }
 
         this.handleEmailChange = this.handleEmailChange.bind(this);
@@ -22,26 +30,60 @@ class SignUpPage extends React.Component {
     }
 
     handleEmailChange(event) {
+        event.preventDefault();
+
+        const email = event.target.value;
+
+        let errors = this.state.errors;
+        errors.email = Validation.emailIsValid(email);
+        
         this.setState({
-            email: event.target.value
+            email: email,
+            errors : errors
         });
     }
 
     handleUsernameChange(event) {
+        event.preventDefault();
+
+        const username = event.target.value;
+
+        let errors = this.state.errors;
+        errors.username = Validation.usernameIsValid(username)
+
         this.setState({
-            username: event.target.value
+            username: username,
+            errors : errors
         });
     }
 
     handlePasswordChange(event) {
+        event.preventDefault();
+
+        const password = event.target.value;
+
+        let errors = this.state.errors;
+        errors.password = Validation.passwordIsValid(password);
+
         this.setState({
-            password: event.target.value
+            password: event.target.value,
+            errors: errors
         });
     }
 
     handleConfirmPasswordChange(event) {
+        event.preventDefault();
+
+        const confirmPassword = event.target.value;
+
+        let errors = this.state.errors;
+        if (!(this.state.password === confirmPassword)) {
+            errors.confirmPassword = 'The confirmation does not match'
+        }
+
         this.setState({
-            confirmPassword: event.target.value
+            confirmPassword: confirmPassword,
+            errors: errors
         });
     }
 
@@ -69,22 +111,40 @@ class SignUpPage extends React.Component {
                 <Link to='/'>Home</Link><br/>
 
                 Sign Up
-                <form onSubmit={this.handleSignUp}>
-                    <input type='text'
+                <p>All fields are required</p>
+                <form onSubmit={this.handleSignUp} autoComplete='off'>
+                    <label for='email'>Email: </label><br/>
+                    <input id='email'
+                           type='email'
                            placeholder='email'
                            value={this.state.email}
+                           autoComplete='off'
+                           required
                            onChange={this.handleEmailChange}/><br/>
-                    <input type='text'
+                    <span>{this.state.errors.email}</span><br/>
+                    <label for='username'>Username: </label><br/>
+                    <input id='username'
+                           type='text'
                            placeholder='username'
                            value={this.state.username}
+                           autoComplete='off'
+                           required
                            onChange={this.handleUsernameChange}/><br/>
-                    <input type='password'
+                    <span>{this.state.errors.username}</span><br/>
+                    <label for='password'>Password: </label><br/>
+                    <input id='password'
+                           type='password'
                            placeholder='password'
                            value={this.state.password}
-                           onChange={this.handlePasswordChange}/><br/>
-                    <input type='password'
+                           autoComplete='new-password'
+                           required
+                           onChange={this.handlePasswordChange}/>
+                    <input aria-label='confirmPassword'
+                           type='password'
                            placeholder='confirm password'
                            value={this.state.confirmPassword}
+                           autoComplete='new-password'
+                           required
                            onChange={this.handleConfirmPasswordChange}/><br/>
                     <input type='submit'/>
                 </form>
